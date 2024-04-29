b0cbcf86409facb5c96d74a308eec1b23a03ded1
fjpolo <fjpolo@gmail.com>
2024-04-29 16:56:24 +0200

    Add formal verification for NESGamepad

--- src/NESGamepad.v
+++ src/NESGamepad.v
@@ -4,7 +4,7 @@
 
 module NESGamepad(
 		input i_clk,
-        input i_rst,
+        input i_rst_n,
 		// Device connections
 		output o_data_clock,
 		output o_data_latch,
@@ -66,7 +66,7 @@ module NESGamepad(
 
     // Handle 60Hz clock counter
 	always @(posedge i_clk) begin
-        if(!i_rst) begin
+        if(!i_rst_n) begin
 			clock_counter_60Hz <= 0;
 		end else begin
 			if(clock_counter_60Hz < (2 * COUNTER_60Hz)) begin
@@ -82,7 +82,7 @@ module NESGamepad(
 
 	// Handle 120uS clock counter
 	always @(posedge i_clk) begin
-		if(!i_rst) begin
+		if(!i_rst_n) begin
 			clock_counter_120uS <= 0;
 			cycle_stage <= 1;
             data <= 8'h00;  
@@ -145,30 +145,96 @@ module NESGamepad(
 	//
 	`ifdef	FORMAL
 
+		`ifdef NESGAMEPAD
+			`define	ASSUME	assume
+		`else
+			`define	ASSUME	assert
+		`endif
+
+    // f_past_valid
+	reg	f_past_valid;
+	initial	f_past_valid = 1'b0;
+	always @(posedge i_clk)
+		f_past_valid <= 1'b1;
+
 		// Clock 60Hz
 		always @(*)
-			assert(clock_counter_60Hz <= (2 * COUNTER_60Hz));
+			if(f_past_valid)
+				assert(clock_counter_60Hz <= (2 * COUNTER_60Hz));
 
 		// Clock 120uS
 		always @(*)
-			assert(clock_counter_120uS <= (2 * COUNTER_120uS));
+			if(f_past_valid)
+				assert(clock_counter_120uS <= (2 * COUNTER_120uS));
 
-		// Cycle stage
-		reg	f_valid_state;
-		always @(*) begin
-			f_valid_state = 0;
-			case(cycle_stage)
-				9'h01: f_valid_state = 1'b1;
-				9'h02: f_valid_state = 1'b1;
-				9'h04: f_valid_state = 1'b1;
-				9'h08: f_valid_state = 1'b1;
-				9'h10: f_valid_state = 1'b1;
-				9'h20: f_valid_state = 1'b1;
-				9'h40: f_valid_state = 1'b1;
-				9'h80: f_valid_state = 1'b1;
-				9'h100: f_valid_state = 1'b1;
-			endcase
-			assert(f_valid_state);
+		// Prove that reset works
+		always @(posedge i_clk) begin
+			if((f_past_valid)&&($past(f_past_valid)&&(!$past(i_rst_n))&&(i_rst_n))) begin
+				assert(clock_counter_120uS == 0);
+				assert(cycle_stage == 1);
+				assert(data == 8'h00);
+				assert(button_state == 8'h00);
+			end
+		end		
+
+		// // Prove that clock_120uS is assigned correctly
+		// always @(*)
+		// 	if(f_past_valid)
+		// 		assert(clock_120uS == (clock_counter_120uS > 0) && (clock_counter_120uS <= COUNTER_120uS)); // Don't, too many ticks
+
+		// Prove that cycle_stage is always only one bit active
+		always @(*)
+			if(f_past_valid)
+				assert((cycle_stage == 0)||(cycle_stage == (1 << 0))||(cycle_stage == (1 << 1))||(cycle_stage == (1 << 2))||(cycle_stage == (1 << 3))||(cycle_stage == (1 << 4))||(cycle_stage == (1 << 5))||(cycle_stage == (1 << 6))||(cycle_stage == (1 << 7))||(cycle_stage == (1 << 8))||(cycle_stage == (1 << 9)));
+
+		always @(*)
+			if(f_past_valid)
+				assert(latch_state == (cycle_stage[0] & (clock_counter_60Hz <= (2 * NUMBER_OF_STATES * COUNTER_120uS + NUMBER_OF_STATES))));
+    
+		always @(*)
+			if(f_past_valid)
+				assert(data_state == (cycle_stage[1] 
+									| cycle_stage[2] 
+									| cycle_stage[3] 
+									| cycle_stage[4] 
+									| cycle_stage[5] 
+									| cycle_stage[6] 
+									| cycle_stage[7]
+									| cycle_stage[8]));	
+    	
+		always @(*)
+			if(f_past_valid)
+				assert(write_state == cycle_stage[9]);
+
+		//
+		// Contract
+		//
+		initial assert(cycle_stage == 1);
+		initial assert(data == 8'h00);
+		initial assert(button_state == 8'h00);
+		initial assert(clock_counter_60Hz == 0);
+		initial assert(clock_counter_120uS == 0);
+		always @(posedge i_clk) begin
+			if((f_past_valid)&&($past(f_past_valid))&&(i_rst_n)&&($past(clock_120uS))) begin
+				if(clock_120uS) begin	// They depend on clock_120uS
+					if($past(latch_state)) begin
+						assert(data == 8'h00);
+					end else if($past(data_state)) begin
+						case($past(cycle_stage))
+							(1 << 1): assert(data[0] == !$past(i_serial_data));    // A
+							(1 << 2): assert(data[1] == !$past(i_serial_data));    // B
+							(1 << 3): assert(data[2] == !$past(i_serial_data));    // Select
+							(1 << 4): assert(data[3] == !$past(i_serial_data));    // Start
+							(1 << 5): assert(data[4] == !$past(i_serial_data));    // Up
+							(1 << 6): assert(data[5] == !$past(i_serial_data));    // Down
+							(1 << 7): assert(data[6] == !$past(i_serial_data));    // Left
+							(1 << 8): assert(data[7] == !$past(i_serial_data));    // Right
+						endcase
+					end else if($past(write_state)) begin
+						assert(button_state == $past(data));
+					end
+				end
+			end
 		end
 
 	`endif
--- src/run_formal_verification.sh
+++ src/run_formal_verification.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# Start
+echo "Running Formal Verification..."
+
 # Run uart_tx_V2 Formal Verification
-# echo "Running Formal Verification..."
-# echo "    uart_tx_V2.v"
+echo "    uart_tx_V2.v"
 sby -f nestang_formal.sby uart_tx_V2
+
+# Run NESGamepad Formal Verification
+echo "    NESGamepad.v"
+sby -f nestang_formal.sby NESGamepad
