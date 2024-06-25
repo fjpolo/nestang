622689afb30bb774ffbbcfdccef67dec0df93b39
fjpolo <fjpolo@gmail.com>
2024-06-25 16:40:40 +0200

    Start working on TB

--- test/test_apu.py
+++ test/test_apu.py
@@ -12,8 +12,25 @@ from cocotb.triggers import RisingEdge
 async def apu_simple_test(dut):
     """Try accessing the design."""
 
-    for cycle in range(10):
-        dut.clk.value = 0
-        await Timer(1, units="ns")
-        dut.clk.value = 1
-        await Timer(1, units="ns")
+    # Set initial values
+    dut.phi1 = 0
+    dut.aclk1
+    dut.aclk1_d
+    dut.reset = 0
+    dut.cold_reset = 0
+    dut.allow_us = 0
+    dut.write
+    dut.LenCtr_Clock
+    dut.LinCtr_Clock
+    dut.Enabled
+    dut.Addr      # 2bit
+    dut.DIN       # 8bit
+    dut.lc_load   # 8bit
+    # outputs
+    # [3:0] dut.Sample
+    # dut.IsNonZero
+
+    # Create a 10us period clock on port clk
+    clock = Clock(dut.clk, 10, units="us")
+    # Start the clock. Start it low to avoid issues on the first RisingEdge
+    cocotb.start_soon(clock.start(start_high=False))
