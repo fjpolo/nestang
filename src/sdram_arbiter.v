37fb28e187b070210bc7452985eb811c82a5162b
fjpolo <fjpolo@gmail.com>
2024-11-08 12:43:29 +0100

    Change regs to wires because if we insert regs in sdram_arbiter, then we have one clock missmatch

--- src/sdram_arbiter.v
+++ src/sdram_arbiter.v
@@ -34,10 +34,10 @@ module sdram_arbiter #(
     parameter [4:0]   T_RC = 4'd6      // 63ns, ref/active to ref/active
 ) 
 (    
-	inout   reg     [SDRAM_DATA_WIDTH-1:0]      SDRAM_DQ,   // 16 bit bidirectional data bus
+	inout   wire    [SDRAM_DATA_WIDTH-1:0]      SDRAM_DQ,   // 16 bit bidirectional data bus
 	output  wire    [SDRAM_ROW_WIDTH-1:0]       SDRAM_A,    // 13 bit multiplexed address bus
-	output  reg     [SDRAM_DATA_WIDTH/8-1:0]    SDRAM_DQM,  // two byte masks
-	output  reg     [1:0]                       SDRAM_BA,   // two banks
+	output  wire    [SDRAM_DATA_WIDTH/8-1:0]    SDRAM_DQM,  // two byte masks
+	output  wire    [1:0]                       SDRAM_BA,   // two banks
 	output  wire                                SDRAM_nCS,  // a single chip select
 	output  wire                                SDRAM_nWE,  // write enable
 	output  wire                                SDRAM_nRAS, // row address select
@@ -48,28 +48,28 @@ module sdram_arbiter #(
 	input   wire                                clk,        // sdram clock
 	input   wire                                resetn,
     input   wire                                clkref,
-    output  reg                                 busy,
+    output  wire                                busy,
 
 	input   wire    [21:0]                      addrA,      // 4MB, bank 0/1
 	input   wire                                weA,        // ppu requests write
 	input   wire    [7:0]                       dinA,       // data input from cpu
 	input   wire                                oeA,        // ppu requests data
-	output  reg     [7:0]                       doutA,      // data output to cpu
+	output  wire    [7:0]                       doutA,      // data output to cpu
 
 	input   wire    [21:0]                      addrB,      // 4MB, bank 0/1
 	input   wire                                weB,        // cpu requests write
 	input   wire    [7:0]                       dinB,       // data input from ppu
 	input   wire                                oeB,        // cpu requests data
-	output  reg     [7:0]                       doutB,      // data output to ppu
+	output  wire    [7:0]                       doutB,      // data output to ppu
 
     // RISC-V softcore
     input   wire    [22:0]      	            rv_addr,      // 2MB RV memory space, bank 2
     input   wire                                rv_word,
     input   wire    [15:0]      	            rv_din,       // 16-bit accesses
     input   wire    [1:0]       	            rv_ds,
-    output  reg     [15:0]      	            rv_dout,
+    output  wire    [15:0]      	            rv_dout,
     input   wire                                rv_req,
-    output  reg                                 rv_req_ack,   // ready for new requests. read data available on NEXT mclk
+    output  wire                                rv_req_ack,   // ready for new requests. read data available on NEXT mclk
     input   wire                                rv_we,
 
     // WRAM
@@ -79,24 +79,42 @@ module sdram_arbiter #(
 reg [7:0] r_dout_cpu;
 reg [16:0] r_dout_rv;
 sdram_nes sdram (
-    .clk(clk), .clkref(clkref), .resetn(resetn), .busy(busy),
+    .clk(clk), 
+    .clkref(clkref), 
+    .resetn(resetn), 
+    .busy(busy),
 
-    .SDRAM_DQ(SDRAM_DQ), .SDRAM_A(SDRAM_A), .SDRAM_BA(SDRAM_BA), 
-    .SDRAM_nCS(SDRAM_nCS), .SDRAM_nWE(SDRAM_nWE), .SDRAM_nRAS(SDRAM_nRAS), 
-    .SDRAM_nCAS(SDRAM_nCAS), .SDRAM_CKE(SDRAM_CKE), .SDRAM_DQM(SDRAM_DQM), 
+    .SDRAM_DQ(SDRAM_DQ), 
+    .SDRAM_A(SDRAM_A), 
+    .SDRAM_BA(SDRAM_BA), 
+    .SDRAM_nCS(SDRAM_nCS), 
+    .SDRAM_nWE(SDRAM_nWE), 
+    .SDRAM_nRAS(SDRAM_nRAS), 
+    .SDRAM_nCAS(SDRAM_nCAS), 
+    .SDRAM_CKE(SDRAM_CKE), 
+    .SDRAM_DQM(SDRAM_DQM), 
 
     // PPU
-    .addrA(addrA), .weA(weA), .dinA(dinA),
-    .oeA(oeA), .doutA(doutA),
+    .addrA(addrA), 
+    .weA(weA), 
+    .dinA(dinA),
+    .oeA(oeA), 
+    .doutA(doutA),
 
     // CPU
-    .addrB(addrB), .weB(weB),
+    .addrB(addrB), 
+    .weB(weB),
     .dinB(dinB),
     .oeB(oeB),
 
     // IOSys risc-v softcore
-    .rv_addr({rv_addr[20:2], rv_word}), .rv_din(rv_din), 
-    .rv_ds(rv_ds), .rv_dout(rv_dout), .rv_req(rv_req), .rv_req_ack(rv_req_ack), .rv_we(rv_we)
+    .rv_addr({rv_addr[20:2], rv_word}), 
+    .rv_din(rv_din), 
+    .rv_ds(rv_ds), 
+    .rv_dout(rv_dout), 
+    .rv_req(rv_req), 
+    .rv_req_ack(rv_req_ack), 
+    .rv_we(rv_we)
 );
 
 endmodule
