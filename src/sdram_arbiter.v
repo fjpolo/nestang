ec728f61b6da363461734d0fb0286ddcbc9af912
fjpolo <fjpolo@gmail.com>
2024-11-10 17:00:08 +0100

    - Fix o_sdram_busy
    - Add some scope files

--- src/sdram_arbiter.v
+++ src/sdram_arbiter.v
@@ -20,51 +20,51 @@
 /*  Some assumptions:
 /*      - rv_req_ack comes from sdram_nes module, we only replace wv_dout
 *******************************************************************/
-//`default_nettype none
+`default_nettype none
 import configPackage::*;
 
 module sdram_arbiter(    
-    input   wire    [0:0]                       i_clk,
-    input   wire    [0:0]                       i_clkref,
-    input   wire    [0:0]                       i_resetn,
-    output  wire    [0:0]                       o_sdram_busy,
+    input   wire                                i_clk,
+    input   wire                                i_clkref,
+    input   wire                                i_resetn,
+    output  wire                                o_sdram_busy,
 
     inout   wire    [(SDRAM_DATA_WIDTH-1):0]    io_sdram_dq,
     output  wire    [(SDRAM_DATA_WIDTH-1):0]    o_sdram_addr,
     output  wire    [1:0]                       o_sdram_ba,
-    output  wire    [0:0]                       o_sdram_cs_n,
-    output  wire    [0:0]                       o_sdram_wen_n,
-    output  wire    [0:0]                       o_sdram_ras_n,
-    output  wire    [0:0]                       o_sdram_cas_n,
-    output  wire    [0:0]                       o_sdram_cke,
+    output  wire                                o_sdram_cs_n,
+    output  wire                                o_sdram_wen_n,
+    output  wire                                o_sdram_ras_n,
+    output  wire                                o_sdram_cas_n,
+    output  wire                                o_sdram_cke,
     output  wire    [SDRAM_DATA_WIDTH/8-1:0]    o_sdram_dqm,
 
     // PPU
     input   wire    [21:0]                      i_memory_addr_ppu,
-    input   wire    [0:0]                       i_memory_write_ppu,
+    input   wire                                i_memory_write_ppu,
     input   wire    [7:0]                       i_memory_sdram_din_ppu_dout,
-    input   wire    [0:0]                       i_memory_read_ppu,
+    input   wire                                i_memory_read_ppu,
     output  wire    [7:0]                       o_memory_sdram_dout_ppu_din,
 
     // CPU
-    input   wire    [0:0]                       i_rom_loading,
+    input   wire                                i_rom_loading,
     input   wire    [21:0]                      i_loader_addr_mem,
-    input   wire    [0:0]                       i_loader_write_mem,
+    input   wire                                i_loader_write_mem,
     input   wire    [21:0]                      i_memory_addr_cpu,
-    input   wire    [0:0]                       i_memory_write_cpu,
+    input   wire                                i_memory_write_cpu,
     input   wire    [7:0]                       i_loader_write_data_mem,
     input   wire    [7:0]                       i_memory_din_sdram_cpu_dout,
-    input   wire    [0:0]                       i_memory_read_cpu,
+    input   wire                                i_memory_read_cpu,
     output  wire    [7:0]                       o_memory_dout_sdram_cpu_din,
 
     // IOSys risc-v softcore
     input   wire    [22:0]                      i_rv_addr, 
-    input   wire    [0:0]                       i_rv_word,
+    input   wire                                i_rv_word,
     input   wire    [31:0]                      i_rv_wdata,
     input   wire    [1:0]                       i_rv_ds,
     output  wire    [15:0]                      o_rv_dout,
-    input   wire    [0:0]                       i_rv_req, 
-    output  wire    [0:0]                       o_rv_req_ack,
+    input   wire                                i_rv_req, 
+    output  wire                                o_rv_req_ack,
     input   wire    [3:0]                       i_rv_wstrb,
 
     // WRAM
@@ -76,7 +76,7 @@ sdram_nes sdram (
     .clk(i_clk),
     .clkref(i_clkref),
     .resetn(i_resetn),
-    .busy(i_sdram_busy),
+    .busy(o_sdram_busy),
     // SDRAM
     .SDRAM_DQ(io_sdram_dq),
     .SDRAM_A(o_sdram_addr),
